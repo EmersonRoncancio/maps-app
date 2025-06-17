0674cf634a99461c953f9c431fd9c01a66b9287c
Emerson Roncancio <roncancioavila@gmail.com>
2025-06-17 02:27:16 -0500

    configuracion del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    loadComponent: () =>
+      import('./pages/fullscreen-map-page/fullscreen-map-page.component').then(
+        (m) => m.FullscreenMapPageComponent
+      ),
+    title: 'Fullscreen Map',
+  },
+  {
+    path: 'markers',
+    loadComponent: () =>
+      import('./pages/markers-page/markers-page.component').then(
+        (m) => m.MarkersPageComponent
+      ),
+    title: 'Markers Map',
+  },
+  {
+    path: 'houses',
+    loadComponent: () =>
+      import('./pages/houses-page/houses-page.component').then(
+        (m) => m.HousesPageComponent
+      ),
+    title: 'Houses Map',
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, linkedSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected title = 'maps-app';
+
+  theme = linkedSignal(() => {
+    const isDarkMode = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    if (isDarkMode) {
+      return 'dark';
+    }
+    return 'light';
+  });
+
+  themeEffect = effect(() => {
+    const isDarkMode = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    console.log(isDarkMode);
+
+    if (this.theme() === 'dark') {
+      console.log('Dark mode is enabled');
+      document.documentElement.setAttribute('data-theme', 'forest');
+    } else {
+      console.log('Dark mode is disabled');
+      document.documentElement.setAttribute('data-theme', 'cupcake');
+    }
+  });
+
+  darkTheme() {
+    this.theme.set('dark');
+  }
+
+  lightTheme() {
+    this.theme.set('light');
+  }
 }
